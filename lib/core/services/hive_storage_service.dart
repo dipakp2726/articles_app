dcf2000149f583e208ff1c443235b82de5994986
Dipak <dipakp2726@gmail.com>
2022-10-15 15:49:52 +0530

    feat : added storage service

---
+++ lib/core/services/hive_storage_service.dart
@@ -0,0 +1,50 @@
+import 'package:articles_app/core/services/storage_service.dart';
+import 'package:hive_flutter/hive_flutter.dart';
+
+class HiveStorageService implements StorageService {
+  late Box hiveBox;
+
+  Future<void> openBox([String boxName = 'ARTICLES_APP']) async {
+    hiveBox = await Hive.openBox(boxName);
+  }
+
+  @override
+  Future<void> init() async {
+    await openBox();
+  }
+
+  @override
+  Future<void> remove(String key) async {
+    hiveBox.delete(key);
+  }
+
+  @override
+  dynamic get(String key) {
+    return hiveBox.get(key);
+  }
+
+  @override
+  dynamic getAll() {
+    return hiveBox.values.toList();
+  }
+
+  @override
+  bool has(String key) {
+    return hiveBox.containsKey(key);
+  }
+
+  @override
+  Future<void> set(String? key, dynamic data) async {
+    hiveBox.put(key, data);
+  }
+
+  @override
+  Future<void> clear() async {
+    await hiveBox.clear();
+  }
+
+  @override
+  Future<void> close() async {
+    await hiveBox.close();
+  }
+}
